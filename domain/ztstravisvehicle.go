df40590ef1d33ca1167bfba3d9286e559ff2fb31
adeard <adeardian1994@gmail.com>
2023-06-30 17:02:48 +0700

    add ztsvehicle schema

--- domain/ztstravisvehicle.go
+++ domain/ztstravisvehicle.go
@@ -10,14 +10,21 @@ type ZtsTravisVehicleRequest struct {
 }
 
 type ZtsTravisVehicle struct {
-	VendorID    string `json:"vendor_id"`
-	VehicleID   string `json:"vehicle_id"`
-	VehicleNo   string `json:"vehicle_no"`
-	VehicleType string `json:"vehicle_type"`
-	UserID      string `json:"user_id"`
-	Udate       string `json:"udate"`
+	VendorID    string `json:"vendor_id" gorm:"column:VendorID"`
+	VehicleID   string `json:"vehicle_id" gorm:"column:VehicleID"`
+	VehicleNo   string `json:"vehicle_no" gorm:"column:VehicleNo"`
+	VehicleType string `json:"vehicle_type" gorm:"column:VehicleType"`
+	UserID      string `json:"user_id" gorm:"column:UserID"`
+	Udate       string `json:"udate" gorm:"column:Udate"`
 }
 
 func (ZtsTravisVehicle) TableName() string {
 	return "ZTS_Travis_Vehicle"
+}
+
+type ZtsTravisVehicleUpdateRequest struct {
+	VehicleNo   string `json:"vehicle_no"`
+	VehicleType string `json:"vehicle_type"`
+	UserID      string `json:"user_id"`
+	Udate       string `json:"udate"`
 }
